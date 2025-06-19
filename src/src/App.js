a5afbb77ca68ae9f6d3c7c5e27754dafc5ffe2c3
leedjah <cultivenou972@gmail.com>
2025-06-19 11:58:39 -0400

    Create App.js

---
+++ src/src/App.js
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div className="app">
+      <h1>Bienvenue sur Cultivé'Nou</h1>
+      <p>Ceci est un espace client de démonstration.</p>
+    </div>
+  );
+}
+
+export default App;
