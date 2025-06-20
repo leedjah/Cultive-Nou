add4192da56b5c620924cd7e8bbc13a73f6d37f7
leedjah <cultivenou972@gmail.com>
2025-06-20 10:57:21 -0400

    Create Client.jsx

---
+++ src/Espace/Client.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const EspaceClient = () => {
+  return (
+    <div>
+      <h2>Bienvenue dans l’espace client Cultivé’Nou</h2>
+      <p>Ici vous verrez vos productions, contrats et votre profil.</p>
+    </div>
+  );
+};
+
+export default EspaceClient;
