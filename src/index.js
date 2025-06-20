d234a5a7d4aed57ee1a747b112602b8af8556e57
leedjah <cultivenou972@gmail.com>
2025-06-20 12:50:15 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
-import EspaceClient from "./EspaceClient";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
 
-function App() {
-  return (
-    <div>
-      <EspaceClient />
-    </div>
-  );
-}
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
