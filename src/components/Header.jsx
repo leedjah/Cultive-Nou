84633998e6928bc015f16a512ae297cc6c81f4c8
leedjah <cultivenou972@gmail.com>
2025-06-20 20:02:38 -0400

    Create Header.jsx

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Header() {
+  return (
+    <header className="bg-green-700 text-white p-4 text-center">
+      <h1 className="text-2xl font-bold">Cultivé'Nou</h1>
+      <p>Investissez dans l'agriculture locale et durable</p>
+    </header>
+  );
+}
