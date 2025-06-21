b531d6b8b8b75e642b0120acee4da7487e07d4d8
leedjah <cultivenou972@gmail.com>
2025-06-20 20:21:00 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Home from './pages/Home';
+import Parrainage from './pages/Parrainage';
+import Contact from './pages/Contact';
+
 function App() {
   return (
-    <div>
-      <h1>Bienvenue sur Cultivé'Nou</h1>
-    </div>
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/parrainage" element={<Parrainage />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+      <Footer />
+    </Router>
   );
 }
 
