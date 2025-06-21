05a2e1e60822ec48997d7bbee0c3329057beeebf
leedjah <cultivenou972@gmail.com>
2025-06-20 20:03:52 -0400

    Create Footer.jsx

---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Footer() {
+  return (
+    <footer className="bg-gray-800 text-white p-4 text-center">
+      <p>&copy; 2025 Cultivé'Nou. Tous droits réservés.</p>
+    </footer>
+  );
+}
